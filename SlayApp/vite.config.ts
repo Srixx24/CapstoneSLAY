6bee798cb1a2d2e6fb30d6d0f5574d06c5846a6d
“AAndrews-1982” <altonandrews@icloud.com>
2025-04-17 09:48:03 -0500

    compiled and updated vite

--- SlayApp/vite.config.ts
+++ SlayApp/vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/CapstoneSLAY/", // Repo name Required for GitHub Pages
   plugins: [react()],
   server: {
     port: 5173,
